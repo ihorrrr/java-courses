9aebdbe7fed2d9ce49ba0362b0fbe8ed182402ce
root <Ihor.1984com@gmail.com>
2015-09-02 17:52:25 +0300

    6 coment

--- C.java
+++ C.java
@@ -1,6 +1,6 @@
 public class C { 
     public static void main(String[] args) { 
-        System.out.println(" Right 4 ");
+        System.out.println(" Right 6 ");
 		
 		int first = Integer.valueOf(args[0]);
 		int second = Integer.valueOf(args[1]);
