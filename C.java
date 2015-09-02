c063e44658beb9e6f1a5c3a3ae1a05f1f476a02c
root <Ihor.1984com@gmail.com>
2015-09-02 16:46:24 +0300

    5 coment

---
+++ C.java
@@ -0,0 +1,10 @@
+public class C { 
+    public static void main(String[] args) { 
+        System.out.println(" Right 4 ");
+		
+		int first = Integer.valueOf(arg[0]);
+		int second = Integer.valueOf(arg[1]);
+		int sum = first + second;
+		System.out.println("sun =" + sum);
+    } 
+}
