97cf1959959743778173460c0dab448267a37d59
root <Ihor.1984com@gmail.com>
2015-09-02 18:33:40 +0300

    6 coment

--- C.java
+++ C.java
@@ -5,6 +5,6 @@ public class C {
 		int first = Integer.valueOf(args[0]);
 		int second = Integer.valueOf(args[1]);
 		int sum = first - second;
-		System.out.println("sun =" + sum);
+		System.out.println("sun = " + sum);
     } 
 }
