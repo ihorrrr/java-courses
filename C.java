d231e4308cc2cfb4ab8c01adf249109ad8b2052a
root <Ihor.1984com@gmail.com>
2015-09-02 17:37:04 +0300

    6 coment

--- C.java
+++ C.java
@@ -2,9 +2,9 @@ public class C {
     public static void main(String[] args) { 
         System.out.println(" Right 4 ");
 		
-		int first = Integer.valueOf(arg[0]);
-		int second = Integer.valueOf(arg[1]);
-		int sum = first + second;
+		int first = Integer.valueOf(args[0]);
+		int second = Integer.valueOf(args[1]);
+		int sum = first - second;
 		System.out.println("sun =" + sum);
     } 
 }
